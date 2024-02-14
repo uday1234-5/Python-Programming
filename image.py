8daba32d5c8637f243c9194c0319a3cfd042ce4b
Uday Kushwah <142593408+uday1234-5@users.noreply.github.com>
2024-02-14 09:39:41 +0530

    Update

---
+++ image.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+root.geometry("1000x70")
+root.title("My first App")
+root.maxsize(400,700)
+root.minsize(100,200)
+
+photo = PhotoImage(file="ima.jpeg")
+
+# photo.pack()
+lb = Label(image=photo)
+lb.pack()
+
+root.mainloop()
